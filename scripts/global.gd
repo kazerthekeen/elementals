9e81c125cb4242abe3a7e97f8ec2f7b88eea2c39
Ezra <ezradudden@gmail.com>
2019-08-16 10:05:10 -0600

    changes

---
+++ scripts/global.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var COLOR_AIR = Color(0, 1, .5)
+var COLOR_FIRE = Color(1, 0, 0)
+var COLOR_WATER = Color(0, .375, 1)
+var COLOR_EARTH = Color( 1, .5, 0)
--- scripts/obsidian.gd
+++ scripts/obsidian.gd
@@ -1,11 +1,40 @@
 extends StaticBody2D
-
-var time = 0.0
+export var fire =10
+export var water = 10
+export var earth = 10
+export var air = 10
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	time = randf()*10
+	pass
 	
+func check_charge():
+	if fire != water or fire != earth or fire!= air :
+		$Timer.start()
+		
+	var val = max(fire, water)
+	val = max(val, air)
+	val = max(val, earth)
+	
+	match val : 
+		fire: 
+			$Sprite.modulate(COLOR_FIRE)
+		water: 
+			$Sprite.modulate(COLOR_WATER)
+		earth: 
+			$Sprite.modulate(COLOR_EARTH)
+		air:
+			$Sprite.modulate(COLOR_AIR)
+		_:
+			pass
+	
+func elemental_charge(dict):
+	fire += dict["fire"]/10
+	water += dict["water"]/10
+	earth += dict["earth"]/10
+	air += dict["air"]/10
+	self.check_charge()
+
 
 func _on_Timer_timeout():
 	pass # Replace with function body.
